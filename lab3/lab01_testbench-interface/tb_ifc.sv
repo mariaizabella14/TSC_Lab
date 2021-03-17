3cb7a12d833fe5399155141cfee053a3d2c21c3f
Izabella <izabella_gires@yahoo.com>
2021-03-17 19:59:32 +0200

    Commit lab3 part2

--- lab3/lab01_testbench-interface/tb_ifc.sv
+++ lab3/lab01_testbench-interface/tb_ifc.sv
@@ -1,11 +1,6 @@
 /***********************************************************************
  * A SystemVerilog testbench for an instruction register; This file
  * contains the interface to connect the testbench to the design
- *
- * SystemVerilog Training Workshop.
- * Copyright 2006, 2013 by Sutherland HDL, Inc.
- * Tualatin, Oregon, USA.  All rights reserved.
- * www.sutherland-hdl.com
  **********************************************************************/
 interface tb_ifc (input logic clk);
   timeunit 1ns/1ns;
@@ -20,8 +15,8 @@ interface tb_ifc (input logic clk);
   operand_t      operand_a, operand_b;
   address_t      write_pointer, read_pointer;
   instruction_t  instruction_word;
-  
-  clocking cb @(posedge clk);
+
+clocking cb @(posedge clk);
     output load_en;
     output reset_n;
     output opcode;
@@ -31,8 +26,5 @@ interface tb_ifc (input logic clk);
     output read_pointer;
     input  instruction_word;
   endclocking;
-
-  // ADD CODE TO DEFINE A CLOCKING BLOCK WITH INPUT AND OUTPUT SKEWS
-
 endinterface: tb_ifc
 
